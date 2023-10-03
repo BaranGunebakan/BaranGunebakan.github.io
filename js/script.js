99910bd9b7aae851334e649f6562f22cc9258ad9
BGunebakan <b.gunebakan@sintlucasedu.nl>
2023-10-03 10:18:48 +0200

    fixed cookies coming back
    now they stay away

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
-// Get references to the buttons and the cookie consent banner
-const acceptAllButton = document.getElementById('acceptAll');
-const acceptNecessaryButton = document.getElementById('acceptNecessary');
-const cookiesBanner = document.querySelector('.cookies');
-
-// Function to close the cookie consent banner
-function closeBanner() {
-    cookiesBanner.style.display = 'none';
-}
-
-// Add click event listeners to the buttons
-acceptAllButton.addEventListener('click', () => {
-    // Set cookies to accept all
-    // You can add your code here to set cookies as needed
-
-    // Close the banner
-    closeBanner();
+let popUp = document.getElementById("cookiePopup");
+//When user clicks the accept button
+document.getElementById("acceptCookie").addEventListener("click", () => {
+  //Create date object
+  let d = new Date();
+  //Increment the current time by 1 minute (cookie will expire after 1 minute)
+  d.setMinutes(2 + d.getMinutes());
+  //Create Cookie withname = myCookieName, value = thisIsMyCookie and expiry time=1 minute
+  document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
+  //Hide the popup
+  popUp.classList.add("hide");
+  popUp.classList.remove("show");
 });
-
-acceptNecessaryButton.addEventListener('click', () => {
-    // Set cookies to accept necessary only
-    // You can add your code here to set necessary cookies
-
-    // Close the banner
-    closeBanner();
-});
+//Check if cookie is already present
+const checkCookie = () => {
+  //Read the cookie and split on "="
+  let input = document.cookie.split("=");
+  //Check for our cookie
+  if (input[0] == "myCookieName") {
+    //Hide the popup
+    popUp.classList.add("hide");
+    popUp.classList.remove("show");
+  } else {
+    //Show the popup
+    popUp.classList.add("show");
+    popUp.classList.remove("hide");
+  }
+};
+//Check if cookie exists when page loads
+window.onload = () => {
+  setTimeout(() => {
+    checkCookie();
+  }, 2000);
+};
